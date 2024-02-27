b4f3f34fb332ac018c4bf6dc86130eb5f925496d
unknown <suren.rayamajhi1425@gmail.com>
2024-02-27 16:19:08 +0545

    netlify added

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+//https://vitejs.dev/config/
 export default defineConfig({
   base: 'https://sonocosoap.com/',
   plugins: [react()],
+  
 })
+// export default defineConfig({
+//   build: {
+//     rollupOptions: {
+//        https://sonocosoap.com/
+//     }
+//   },
+// })
 
 
